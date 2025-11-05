9d25696d00437159df85caebda780d696a4b1a2c
logan <92327020+DevLoggith@users.noreply.github.com>
2025-11-05 13:11:16 -0500

    implement multer for file uploading/parsing
    - configure multer to use memory storage (buffer) and only allow uploading
    1 file at a time
    - create endpoint to parse file info & return json object with necessary
    data

--- index.js
+++ index.js
@@ -5,12 +5,22 @@ import multer from "multer";
 
 dotenv.config();
 const app = express();
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 app.use(cors());
 app.use("/public", express.static(process.cwd() + "/public"));
 
 app.get("/", function (req, res) {
 	res.sendFile(process.cwd() + "/views/index.html");
+});
+
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+	res.json({
+		name: req.file.originalname,
+		type: req.file.mimetype,
+		size: req.file.size,
+	});
 });
 
 const port = process.env.PORT || 3000;
